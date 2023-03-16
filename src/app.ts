b240e09454b9f3b138f828002450466ef54ffff7
Rio Edwards <rioredwards@gmail.com>
2023-03-15 18:58:18 -0700

    Merge code from previous commits to display all libraries used

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import _ from "lodash";
 import "reflect-metadata";
 import { validate } from "class-validator";
 import { Product } from "./product.model";
+import { plainToInstance } from "class-transformer";
 
 const newProd = new Product("", 5.99);
 validate(newProd).then((errors) => {
@@ -11,3 +13,16 @@ validate(newProd).then((errors) => {
     console.log(newProd.getInformation());
   }
 });
+
+console.log(_.shuffle([1, 2, 3, 4, 5]));
+
+const products = [
+  { title: "A Book", price: 12.99 },
+  { title: "A Carpet", price: 99.99 },
+];
+
+const loadedProducts = plainToInstance(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
