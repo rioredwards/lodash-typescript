389cb1eb4a69e2479df21b718c9796c227f152a1
Rio Edwards <rioredwards@gmail.com>
2023-03-15 18:49:01 -0700

    Use the class-validator library which adds decorators for validating data

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,13 @@
 import "reflect-metadata";
-import { plainToInstance } from "class-transformer";
-
+import { validate } from "class-validator";
 import { Product } from "./product.model";
 
-const products = [
-  { title: "A Book", price: 12.99 },
-  { title: "A Carpet", price: 99.99 },
-];
-
-const loadedProducts = plainToInstance(Product, products);
-
-// const loadedProducts = products.map((prod) => {
-//   return new Product(prod.title, prod.price);
-// });
-
-for (const prod of loadedProducts) {
-  console.log(prod.getInformation());
-}
-
-// const p1 = new Product("A Book", 12.99);
-// console.log(p1.getInformation());
+const newProd = new Product("", 5.99);
+validate(newProd).then((errors) => {
+  if (errors.length > 0) {
+    console.log("Validation errors!");
+    console.log(errors);
+  } else {
+    console.log(newProd.getInformation());
+  }
+});
